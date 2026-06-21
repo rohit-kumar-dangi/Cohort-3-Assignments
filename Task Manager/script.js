adf908495d05e168b796eb14be99d32b3edc0118
rohit-kumar-dangi <kumar.rohit.dangi.01@gmail.com>
2026-06-21 23:37:40 +0530

    Implement task addition feature with modal and update styles

---
+++ Task Manager/script.js
@@ -0,0 +1,73 @@
+let add_task = document.querySelector(".add_task");
+let closeBtn = document.querySelector(".close");
+let addForm = document.querySelector(".add_form");
+let taskContainer = document.querySelector(".tasks_container")
+let taskData = JSON.parse(localStorage.getItem("data")) || [];
+let editIndex = null;
+
+closeBtn.addEventListener("click",() => {
+    add_task.style.display = "none";
+});
+
+const showAddTask = () => {
+    add_task.style.display = "flex";
+};
+
+const updateData = (datas)=>{
+    let stringData=JSON.stringify(datas)
+    localStorage.setItem("data",stringData);
+}
+
+const addData = ()=>{
+    event.preventDefault();
+    taskData.push([addForm[0].value,false]);
+    updateData(taskData);
+    addForm[0].value="";
+    add_task.style.display = "none";
+    showTask();
+}
+
+const showTask = () => {
+    let tasks = ``
+    taskData.forEach((element,index) => {
+        tasks += `<div class="task">
+                    <div class="task_name">
+                        <p>${element[0]}</p>
+                    </div>
+                    <div class="task_option">
+                        <button id="delete_btn" onclick="deleteTask(${index})">Delete</button>
+                        <button id="edit_btn" onclick="editTask('${element[0]}',${index})">Edit</button>
+                        <button id="Complete_btn" onclick="completeTask(${index})">Complete</button>
+                    </div>
+                </div>`
+    });
+    taskContainer.innerHTML = tasks;
+};
+
+const deleteTask = (index)=>{
+    taskData.splice(index,1);
+    updateData(taskData);
+    showTask();
+};
+
+
+const editTask = (element,index)=>{
+    showAddTask();
+    addForm[0].value=element;
+    addForm[1].innerHTML="Update";
+    addForm[1].onclick=editData;
+    editIndex=index;
+};
+const editData = ()=>{
+    event.preventDefault();
+    taskData[editIndex][0] = addForm[0].value;
+    editIndex=null;
+    updateData(taskData);
+    addForm[0].value="";
+    addForm[1].innerHTML="+ Add Task";
+    addForm[1].onclick=addData;
+    add_task.style.display = "none";
+    showTask();
+};
+
+showTask();
